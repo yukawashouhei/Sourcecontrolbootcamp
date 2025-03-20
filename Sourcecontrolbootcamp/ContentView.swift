c6305f24f56e506c80079788916eb470f5e5a200
Shohei <yukawa.shouhei@gmail.com>
2025-03-20 16:49:35 +0900

    Initial Commit

---
+++ Sourcecontrolbootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sourcecontrolbootcamp
+//
+//  Created by 湯川昇平 on 2025/03/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sourcecontrolbootcamp/SourcecontrolbootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourcecontrolbootcampApp.swift
+//  Sourcecontrolbootcamp
+//
+//  Created by 湯川昇平 on 2025/03/20.
+//
+
+import SwiftUI
+
+@main
+struct SourcecontrolbootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
